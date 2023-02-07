3ed22561efd0b345579a3d974bacb22406a4a11b
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-07 18:50:15 -0500

    saved 'urls' file in 'comments' app

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -4,6 +4,6 @@ from comments import views
 # <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
-    # path('', views.comments),
-    # path('all/', views.comments_detail),
+    path('', views.comments),
+    path('all/', views.comments_detail),
 ]
