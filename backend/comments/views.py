7f71333c274e95cfa813648e23a1e56c52df31c9
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-08 11:26:32 -0500

    created 'views' file in 'comments' app

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import CommentSerializer
+from rest_framework import status
+from .models import Comments
 
 # Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def comments(request):
+    comments = Comments.objects.all()
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])  # this makes sure the user is registered and logged in
+def comments_detail(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        comments = Comments.objects.filter(user_id=request.user.id)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
