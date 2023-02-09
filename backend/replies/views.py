bf10216b243f75ffd48f80844cac3ac12fbd6c30
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-09 18:56:39 -0500

    created functioning 'GET' request for 'replies'

--- backend/replies/views.py
+++ backend/replies/views.py
@@ -17,9 +17,9 @@ def video_replies(request):
     return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 @permission_classes([IsAuthenticated])  # this makes sure the user is registered and logged in
-def user_replies(request, comment_id):
+def user_replies(request, pk):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
     if request.method == 'POST':
@@ -28,17 +28,10 @@ def user_replies(request, comment_id):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def user_replies(request, pk):
-    comment = get_object_or_404(Reply, pk=pk)
-    serializer = ReplySerializer(comment, data=request.data)
-    if request.method == 'GET':
+    elif request.method == 'GET':
         replies = Reply.objects.filter(comment_id=pk)
         serializer = ReplySerializer(replies, many=True)
         return Response(serializer.data)
-
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated]) 
