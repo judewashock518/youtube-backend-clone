7eef178c61079afff4ec1512fff86d273085e874
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-10 14:33:41 -0500

    added 'comment_id' to 'serializers'

--- backend/replies/serializers.py
+++ backend/replies/serializers.py
@@ -5,5 +5,6 @@ from .models import Reply
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
-        fields = ['id', 'user', 'comment','text']
+        fields = ['id', 'user', 'comment','text','comment_id']
         depth = 1
+    comment_id = serializers.IntegerField(write_only=True)
