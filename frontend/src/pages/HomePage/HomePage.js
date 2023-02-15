20600ad7ab26514e670fa681b8a07bec52ba1a69
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-15 14:51:26 -0500

    obtained key. created 'get' search results and display form. created form for the search bar. ability to search for videos

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import CommentForm from "../../components/CommentForm/CommentForm";
 import CommentList from "../../components/CommentList/CommentList";
 import useAuth from "../../hooks/useAuth";
+import "./HomePage.css";
+import KEY from "../../components/key";
 
 import axios from "axios";
 
@@ -11,18 +13,61 @@ const HomePage = () => {
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   //TODO: Add an AddCars Page to add a car for a logged in user's garage
   const [user, token] = useAuth();
-  const [videoData, setVideoData] = useState();
+  const [videoData, setVideoData] = useState([]);
+  const [query, setQuery] = useState();
+  console.log(query);
 
-  function addNewEntry(entry){
-    let tempEntries = [entry, ...entries];
+  function filterVideoData(event) {
+    event.preventDefault();
+    let response = videoData.filter((video) => {
+        if(video.snippet.title.toLowerCase().includes(query) || 
+          video.snippet.description.toLowerCase().includes(query) ){
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    console.log(response)
+    console.log(query)
+    // props.setVideos(response);
 
-    setEntries(tempEntries);
   }
 
+  useEffect(() => {
+    getVideoData()
+  },[])
+
+
+  async function getVideoData(query="Bob Ross"){
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${query}&key=${KEY}&type=video&maxResults=5&part=snippet`)
+    setVideoData(response.data.items)
+    console.log(response.data.items)
+  }
+
+
   return (
-    <div className="container">
-      <CommentForm addNewEntryProperty={addNewEntry}/>
-      <CommentList parentEntries={entries} />
+    <div className="form-grid">
+      <form onSubmit={filterVideoData} className='form-grid'>
+            <div className='form-group'>
+             <input type='text' 
+             placeholder='search videos...' 
+             className='form-control' value={query} onChange={(event) => setQuery(event.target.value)} />
+            </div>
+            <button type='submit' class='btn btn-primary' style={{'margin-top': '1rem'}}>Search</button>
+      </form>
+      {videoData.map((video) => {
+      return (
+        
+        <div>
+          <p>{video.snippet.title}</p>
+        <img src={video.snippet.thumbnails.medium.url} alt="videos"></img>
+        <p>{video.snippet.description}</p>
+        </div>
+        
+        
+      );
+      })}
     </div>
   );
 }
@@ -31,7 +76,8 @@ export default HomePage;
 
 
 
-
+{/* <CommentForm addNewEntryProperty={addNewEntry}/>
+      <CommentList parentEntries={entries} /> */}
 
 
 // const HomePage = () => {
